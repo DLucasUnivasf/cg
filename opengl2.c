bf75188b2c96dbe00fa95728e229c456502afa6c
danielesreis <danielesreis@bitbucket.org>
2018-02-19 23:59:32 -0300

    rotation and translation done, I guess

--- opengl2.c
+++ opengl2.c
@@ -1,10 +1,12 @@
 #include <math.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <GL/glut.h>
 
+#define PI 3.14159265
+
 GLfloat dx, dy, step;
 GLfloat ang, ang_step;
-GLfloat px, py;
 
 void Inicializa (void)
 {
@@ -12,10 +14,9 @@ void Inicializa (void)
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D (-7.0f, 13.0f, -3.0f, 17.0f);
 
-    dx = dy = ang = px = 0.0;
-    py = 1.0;
+    dx = dy = ang = 0.0;
     ang_step = 2;
-    step = 0.5;
+    step = 0.3;
 }
 
 void DesenhaLabirinto()
@@ -33,23 +34,44 @@ void DesenhaLabirinto()
     glVertex2f(-0.75, 3.5);
     glVertex2f(-4.75, 3.5);
 
+    glVertex2f(0.75, 5.0);
+    glVertex2f(-3.25, 5.0);
+
     glVertex2f(-4.75, 3.5);
     glVertex2f(-4.75, 10.5);
 
+    glVertex2f(-3.25, 5.0);
+    glVertex2f(-3.25, 9.0);
+
     glVertex2f(-4.75, 10.5);
     glVertex2f(5.25, 10.5);
 
+    glVertex2f(-3.25, 9.0);
+    glVertex2f(3.75, 9.0);
+
     glVertex2f(5.25, 10.5);
     glVertex2f(5.25, 7.5);
 
+    glVertex2f(3.75, 9.0);
+    glVertex2f(3.75, 6.0);
+
     glVertex2f(5.25, 7.5);
     glVertex2f(6.25, 7.5);
 
+    glVertex2f(3.75, 6.0);
+    glVertex2f(7.75, 6.0);
+
     glVertex2f(6.25, 7.5);
     glVertex2f(6.25, 12.5);
 
+    glVertex2f(7.75, 6.0);
+    glVertex2f(7.75, 11.0);
+
     glVertex2f(6.25, 12.5);
     glVertex2f(11.25, 12.5);
+
+    glVertex2f(7.75, 11.0);
+    glVertex2f(11.25, 11.0);
 
     glEnd();
 }
@@ -60,11 +82,10 @@ void Desenha(void)
 	glLoadIdentity();
 
     glClear(GL_COLOR_BUFFER_BIT);
-
     DesenhaLabirinto();
 
-    glRotatef(ang, 0.0f, 0.0f, 1.0f);
     glTranslatef(dx, dy, 0.0);
+    glRotatef(ang, 0.0f, 0.0f, 1.0f);
 
     glBegin(GL_TRIANGLES);
     glColor3f(1.0f, 0.0f, 1.0f);
@@ -84,13 +105,58 @@ void Teclado(unsigned char key, int x, int y)
 
 void SetasTeclado(unsigned char key, int x, int y)
 {
+    if (ang < 0) ang += 360;
+    ang = fmod(ang, 360);
     if (key == GLUT_KEY_UP)
-        dy += step;
+    {
+        if (ang >= 0 && ang <= 90)
+        {
+            dy += cos(fabs(ang)*PI/180) * step;
+            dx -= sin(fabs(ang)*PI/180) * step;
+        }
+        else if (ang > 90 && ang <= 180)
+        {
+            dy -= -cos(fabs(ang)*PI/180) * step;
+            dx -= sin(fabs(ang)*PI/180) * step;
+        }
+        else if (ang > 180 && ang <= 270)
+        {
+            dy -= -cos(fabs(ang)*PI/180) * step;
+            dx += -sin(fabs(ang)*PI/180) * step;
+            printf("%f %f\n", -sin(fabs(ang)*PI/180) * step, -cos(fabs(ang)*PI/180) * step);
+        }
+        else if (ang > 270 && ang <= 360)
+        {
+            dy += cos(fabs(ang)*PI/180) * step;
+            dx += -sin(fabs(ang)*PI/180) * step;
+        }
+    }
     else if (key == GLUT_KEY_DOWN)
-        dy -= step;
+    {
+        if (ang >= 0 && ang <= 90)
+        {
+            dy -= cos(ang*PI/180) * step;
+            dx += sin(ang*PI/180) * step;
+            printf("%f %f\n", sin(ang*PI/180) * step, cos(ang*PI/180) * step);
+        }
+        else if (ang > 90 && ang <= 180)
+        {
+            dy += -cos(ang*PI/180) * step;
+            dx += sin(ang*PI/180) * step;
+        }
+        else if (ang > 180 && ang <= 270)
+        {
+            dy += -cos(ang*PI/180) * step;
+            dx -= -sin(ang*PI/180) * step;
+        }
+        else if (ang > 270 && ang <= 360)
+        {
+            dy -= cos(ang*PI/180) * step;
+            dx -= -sin(ang*PI/180) * step;
+        }
+    }
     else if (key == GLUT_KEY_LEFT)
         ang += ang_step;
-
     else if (key == GLUT_KEY_RIGHT)
         ang -= ang_step;
 
