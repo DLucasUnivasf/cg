e982ebbfe9407951acf1461e7968cc0bd752fadc
Daniele Reis <danielesreis@outlook.com>
2018-03-07 18:51:06 -0300

    hit mechanics done

--- projeto_CG/main.c
+++ projeto_CG/main.c
@@ -5,21 +5,40 @@
 #include <math.h>
 #include <GL/glut.h>
 
-GLfloat esq = 0.0f, dir = 50.0f, base = 0.0f, cima = 50.0f;
-GLfloat largura = 500, altura = 500;
-GLint qtd_buracos = 1;
+GLfloat dirg = 100.0f, cimag = 50.0f;
+GLfloat esq = 0.0f, dir = 70.0f, base = 0.0f, cima = 50.0f;
+GLfloat largura = 700, altura = 500;
+int qtd_buracos = 1;
+GLfloat r_buraco, g_buraco, b_buraco;
+GLfloat **buracos_pos;
 
-void Inicializa (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top)
+void Inicializa ()
 {
+    r_buraco = 0.5;
+    g_buraco = 0.5;
+    b_buraco = 0.5;
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D (esq, dir, base, cima);
+    gluOrtho2D (esq, dirg, base, cimag);
 
+    //srand(time(NULL));
+}
+
+void salvaPosicao(GLfloat pos_x, GLfloat pos_y, GLfloat larg_buraco, GLfloat alt_buraco, int ctr)
+{
+    buracos_pos[ctr][0] = pos_x;
+    buracos_pos[ctr][1] = pos_x + larg_buraco;
+    buracos_pos[ctr][2] = pos_y;
+    buracos_pos[ctr][3] = pos_y + alt_buraco;
 }
 
 void CalculaBuracos(void)
 {
-    int i, j;
+    int i, j, ctr = 0;
+    buracos_pos = (GLfloat **)malloc(sizeof(GLfloat *) * qtd_buracos);
+
+    for (i = 0; i < qtd_buracos; i++)
+        buracos_pos[i] = (GLfloat *)malloc(sizeof(GLfloat) * 4);
 
     GLfloat raizf = sqrt(qtd_buracos);
     GLint raizi = raizf, qtd = raizi * raizi;
@@ -42,8 +61,6 @@ void CalculaBuracos(void)
 
     qtdy_aux = (rest_ult_linha == 0) ? raizi + linha_rest : raizi + linha_rest + 1;
 
-    //printf("qtd_buracos %d | raizi %d | qtd %d | rest %d | linh_rest %d | rest_ult_linha %d | qtdy_aux %d | \n", qtd_buracos, raizi, qtd, rest, linha_rest, rest_ult_linha, qtdy_aux);
-
     qtdx_aux = raizi;
     for (i=0; i<qtdy_aux; i++)
     {
@@ -56,14 +73,30 @@ void CalculaBuracos(void)
             pos_y = (float)(i*alt_buraco+(i+1)*dist_buraco_y);
 
             glBegin(GL_POLYGON);
-            glColor3f(0.0f, 1.0f, 0.0f);
+            glColor3f(r_buraco, g_buraco, b_buraco);
             glVertex3f(pos_x, pos_y, 0);
             glVertex3f(pos_x + larg_buraco, pos_y, 0);
             glVertex3f(pos_x + larg_buraco, pos_y + alt_buraco, 0);
             glVertex3f(pos_x, pos_y + alt_buraco, 0);
             glEnd();
+
+            salvaPosicao(pos_x, pos_y, larg_buraco, alt_buraco, ctr);
+            ctr++;
         }
     }
+}
+
+int buracoAcertado(GLfloat x, GLfloat y)
+{
+    int i;
+
+    for (i=0; i<qtd_buracos; i++)
+    {
+        if((x > buracos_pos[i][0] && x < buracos_pos[i][1]) && (y > buracos_pos[i][2] && y < buracos_pos[i][3]))
+            return i;
+    }
+
+    return -1;
 }
 
 void Desenha(void)
@@ -72,6 +105,13 @@ void Desenha(void)
 	glLoadIdentity();
 
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glVertex2f(esq, base);
+    glVertex2f(dir, base);
+    glVertex2f(dir, cima);
+    glVertex2f(esq, cima);
+    glEnd();
     CalculaBuracos();
     glFlush();
 }
@@ -89,7 +129,7 @@ void Teclado(unsigned char key, int x, int y)
     else if (key == 43)
         qtd_buracos++;
     else if (key == 45)
-        qtd_buracos--;
+        qtd_buracos = (qtd_buracos == 1) ? 1 : qtd_buracos-1;
 
     glutPostRedisplay();
 }
@@ -111,12 +151,13 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
     glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-    gluOrtho2D (esq, dir, base, cima);
+    gluOrtho2D (esq, dirg, base, cimag);
 }
 
 void GerenciaMouse(int button, int state, int x, int y)
 {
     GLfloat new_x, new_y, x_aux, y_aux;
+    int pos;
 
     if (button == GLUT_LEFT_BUTTON)
     {
@@ -125,8 +166,10 @@ void GerenciaMouse(int button, int state, int x, int y)
             x_aux = (float)x;
             y_aux = (float)y;
 
-            new_x = (x_aux * (dir - esq))/largura;
-            new_y = (y_aux * (cima - base))/altura;
+            new_x = (x_aux * (dirg - esq))/largura;
+            new_y = cimag - (y_aux * (cimag - base))/altura;
+            pos = buracoAcertado(new_x, new_y);
+            //if(pos != -1) faz algo
         }
     }
 }
@@ -142,7 +185,7 @@ int main(void)
     glutKeyboardFunc(Teclado);
     //glutSpecialFunc(SetasTeclado);
     glutMouseFunc(GerenciaMouse);
-    Inicializa(esq, dir, base, cima);
+    Inicializa();
 
     glutMainLoop();
 }
