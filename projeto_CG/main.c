c3bba8158b021d2747fe61c65e349e699b2de1f5
Daniele Reis <danielesreis@outlook.com>
2018-03-29 13:55:58 -0300

    imprimindo pontos

--- projeto_CG/main.c
+++ projeto_CG/main.c
@@ -5,22 +5,33 @@
 #include <math.h>
 #include <GL/glut.h>
 
-GLfloat dirg = 100.0f, cimag = 50.0f;
-GLfloat esq = 0.0f, dir = 70.0f, base = 0.0f, cima = 50.0f;
-GLfloat largura = 700, altura = 500;
-int qtd_buracos = 1;
 GLfloat r_buraco, g_buraco, b_buraco;
 GLfloat **buracos_pos;
 GLfloat new_x, new_y;
 GLfloat larg_buraco, alt_buraco;
 GLfloat martelo_angulo;
 
-void Inicializa ()
+GLfloat dirg    = 100.0f,
+        cimag   = 50.0f;
+
+GLfloat esq     = 0.0f,
+        dir     = 70.0f,
+        base    = 0.0f,
+        cima    = 50.0f;
+
+GLfloat largura = 700,
+        altura  = 500;
+
+int pontos      = 0;
+int qtd_buracos = 4;
+
+void Inicializa()
 {
-    r_buraco = 0.5;
-    g_buraco = 0.5;
-    b_buraco = 0.5;
-    martelo_angulo = 45;
+    r_buraco        = 0.5;
+    g_buraco        = 0.5;
+    b_buraco        = 0.5;
+    martelo_angulo  = 45;
+
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D (esq, dirg, base, cimag);
@@ -44,27 +55,30 @@ void CalculaBuracos(void)
     for (i = 0; i < qtd_buracos; i++)
         buracos_pos[i] = (GLfloat *)malloc(sizeof(GLfloat) * 4);
 
-    GLfloat raizf = sqrt(qtd_buracos);
-    GLint raizi = raizf, qtd = raizi * raizi;
-    GLint rest = qtd_buracos - qtd;
+    GLfloat raizf   = sqrt(qtd_buracos);
+    GLint raizi     = raizf;
+    GLint qtd       = raizi * raizi;
+    GLint rest      = qtd_buracos - qtd;
+
     GLint linha_rest;
     GLfloat dist_buraco_x, dist_buraco_y;
     GLint qtdx_aux, qtdy_aux, rest_ult_linha;
 
     GLfloat pos_x, pos_y;
 
-    linha_rest = rest / raizi;
-    rest_ult_linha = rest % raizi;
+    linha_rest      = rest / raizi;
+    rest_ult_linha  = rest % raizi;
 
-    larg_buraco = (dir - esq)/(raizi + 1);
-    alt_buraco = (rest_ult_linha == 0) ? (cima - base)/(raizi + linha_rest + 1) : (cima - base)/(raizi + linha_rest + 2);
+    larg_buraco     = (dir - esq)/(raizi + 1);
+    alt_buraco      = (rest_ult_linha == 0) ? (cima - base)/(raizi + linha_rest + 1) : (cima - base)/(raizi + linha_rest + 2);
 
-    dist_buraco_x = larg_buraco / (raizi + 1);
-    dist_buraco_y = (rest_ult_linha == 0) ? alt_buraco / (raizi + linha_rest + 1) : alt_buraco / (raizi + linha_rest + 2);
+    dist_buraco_x   = larg_buraco / (raizi + 1);
+    dist_buraco_y   = (rest_ult_linha == 0) ? alt_buraco / (raizi + linha_rest + 1) : alt_buraco / (raizi + linha_rest + 2);
 
-    qtdy_aux = (rest_ult_linha == 0) ? raizi + linha_rest : raizi + linha_rest + 1;
+    qtdy_aux        = (rest_ult_linha == 0) ? raizi + linha_rest : raizi + linha_rest + 1;
 
-    qtdx_aux = raizi;
+    qtdx_aux        = raizi;
+
     for (i=0; i<qtdy_aux; i++)
     {
         if (i == qtdy_aux - 1)
@@ -95,8 +109,12 @@ int buracoAcertado(GLfloat x, GLfloat y)
 
     for (i=0; i<qtd_buracos; i++)
     {
-        if((x > buracos_pos[i][0] && x < buracos_pos[i][1]) && (y > buracos_pos[i][2] && y < buracos_pos[i][3]))
+        if((x > buracos_pos[i][0] && x < buracos_pos[i][1]) &&
+           (y > buracos_pos[i][2] && y < buracos_pos[i][3]))
+        {
+            pontos += qtd_buracos;
             return i;
+        }
     }
 
     return -1;
@@ -116,9 +134,12 @@ void Desenha(void)
     glVertex2f(dir, cima);
     glVertex2f(esq, cima);
     glEnd();
+    DesenhaPontos();
     CalculaBuracos();
     DesenhaMartelo();
     glFlush();
+
+    glColor3f(1,0.5,0);
 }
 
 void DesenhaMartelo()
@@ -141,10 +162,21 @@ void DesenhaMartelo()
     glEnd();
 }
 
-void DesenhaTextoStroke(void *font, char *string)
+void DesenhaPontos()
 {
+    char *string = "Pontos";
+    char teste[10];
+
+    glPushMatrix();
+    glTranslatef(dir, cimag-6, 0);
+    glScalef(0.07, 0.05, 0.1);
+    glLineWidth(2);
+
 	while(*string)
-		glutStrokeCharacter(font, *string++);
+		glutStrokeCharacter(GLUT_STROKE_ROMAN, *string++);
+
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 void Teclado(unsigned char key, int x, int y)
@@ -152,9 +184,9 @@ void Teclado(unsigned char key, int x, int y)
     if (key == 27)
         exit(0);
     else if (key == 43)
-        qtd_buracos++;
+        qtd_buracos = (qtd_buracos == 25) ? 25 : qtd_buracos+1;
     else if (key == 45)
-        qtd_buracos = (qtd_buracos == 1) ? 1 : qtd_buracos-1;
+        qtd_buracos = (qtd_buracos == 4) ? 4 : qtd_buracos-1;
 
     glutPostRedisplay();
 }
